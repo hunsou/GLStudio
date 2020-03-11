f299a8af4d31f163e1d488b15e8463525e3a0807
hunsou <hunsou@msn.com>
2020-03-11 23:11:53 +0800

    2020-03-11 23:11:53

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git add .
 git commit -m "`date +'%Y-%m-%d %H:%M:%S'`"
-git tag -a v1.0.3 -m "`date +'%Y-%m-%d %H:%M:%S'`"
+git tag -a v1.0.4 -m "`date +'%Y-%m-%d %H:%M:%S'`"
 git push origin master --tags
