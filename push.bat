b5f9b59b2e0699b9867702fdc9be63a808690c40
hunsou <hunsou@msn.com>
2020-03-13 11:35:13 +0800

    2020-03-13 11:35:13

--- push.bat
+++ push.bat
@@ -1,4 +1,5 @@
+@echo off
 git add .
 git commit -m "%date:~6,4%-%date:~0,2%-%date:~3,2% %time:~0,8%"
-rem git push origin master --tags
-git push origin master
+git push origin master --tags
+rem git push origin master
