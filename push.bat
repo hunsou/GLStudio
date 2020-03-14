cfce432372780d9492bbce0ff724e6b4c6262861
hunsou <hunsou@msn.com>
2020-03-14 14:45:23 +0800

    2020-03-14 14:45:23

--- push.bat
+++ push.bat
@@ -2,5 +2,5 @@
 git add .
 git commit -m "%date:~6,4%-%date:~0,2%-%date:~3,2% %time:~0,8%"
 ::git tag -a v1.1.7 -m ""
-git push origin master --tags
-rem git push origin master
+rem git push origin master --tags
+git push origin master
--- push.sh
+++ push.sh
@@ -2,4 +2,5 @@
 git add .
 git commit -m "`date +'%Y-%m-%d %H:%M:%S'`"
 #git tag -a v1.0.`date +'%Y.%m.%d.%H.%M.%S'` -m "`date +'%Y-%m-%d %H:%M:%S'`"
-git push origin master --tags
+#git push origin master --tags
+git push origin master
